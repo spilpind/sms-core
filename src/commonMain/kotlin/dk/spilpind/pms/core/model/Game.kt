8e528b307dc9963d627ee05ecc7c5148ec3d6a6b
Anders Kielsholm <anders@anigif.dk>
2021-06-08 21:48:00 +0200

    Update game structure since it has optional teams and focus is a boolean getter

--- src/commonMain/kotlin/dk/spilpind/pms/core/model/Game.kt
+++ src/commonMain/kotlin/dk/spilpind/pms/core/model/Game.kt
@@ -3,8 +3,8 @@ package dk.spilpind.pms.core.model
 data class Game(
     val gameId: Int?,
     val tournament: Tournament,
-    val teamA: Team,
-    val teamB: Team,
+    val teamA: Team?,
+    val teamB: Team?,
     val description: String,
-    val focus: Boolean
+    val isFocused: Boolean
 )
