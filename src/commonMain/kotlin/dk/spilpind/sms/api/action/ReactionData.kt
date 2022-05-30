243fe878a9f9f601b795974bb494b5e0f827049a
Anders Kielsholm <anders@anigif.dk>
2022-05-30 22:25:57 +0200

    Add missing documentation

--- src/commonMain/kotlin/dk/spilpind/sms/api/action/ReactionData.kt
+++ src/commonMain/kotlin/dk/spilpind/sms/api/action/ReactionData.kt
@@ -3,6 +3,9 @@ package dk.spilpind.sms.api.action
 import dk.spilpind.sms.api.common.Reaction
 import kotlinx.serialization.Serializable
 
+/**
+ * Base of all reactions, including non-successful
+ */
 @Serializable
 sealed class ReactionData {
     abstract val reaction: Reaction
