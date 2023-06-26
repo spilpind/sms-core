d524abe8b98b6803687648af8441abe2166e79e8
Anders Kielsholm <anders@anigif.dk>
2023-06-26 21:02:38 +0200

    Create new status class to be used from the api to indicate status of the service behind the api

---
+++ src/commonMain/kotlin/dk/spilpind/sms/api/core/Status.kt
@@ -0,0 +1,75 @@
+package dk.spilpind.sms.api.core
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+/**
+ * Represents current status of the service running the API. [localizedMessage] can, if provided, can be shown to the
+ * user to indicate what the state is and what they can expect of the service
+ */
+@Serializable
+data class Status(
+    val type: Type,
+    val localizedMessage: String?
+) {
+    @Serializable(with = TypeSerializer::class)
+    enum class Type {
+        /**
+         * The service is fully functional
+         */
+        Running,
+
+        /**
+         * The service is functional, but restricted and thus cannot be used without special access. This is expected
+         * to be temporary
+         */
+        Restricted,
+
+        /**
+         * The service is under maintenance (being upgraded, fixed or alike). This is expected to be temporary
+         */
+        Maintenance,
+
+        /**
+         * The service is in an (unexpected) error state and is not expected to become online again until someone
+         * manually fixes the issue
+         */
+        Error,
+
+        /**
+         * The way of fetching the status was outdated and the client should be updated to match the newest version of
+         * the api
+         */
+        Outdated,
+
+        /**
+         * The state was not possible to deduct as it was unknown to the current state of the core components. This type
+         * should never be used directly as a response from the api
+         */
+        Unknown,
+    }
+
+    private object TypeSerializer : KSerializer<Type> {
+        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
+            serialName = "SmsCore.Status.Type",
+            kind = PrimitiveKind.STRING
+        )
+
+        override fun serialize(encoder: Encoder, value: Type) {
+            val typeName = value.name.lowercase()
+            encoder.encodeString(typeName)
+        }
+
+        override fun deserialize(decoder: Decoder): Type {
+            val typeName = decoder.decodeString()
+            return Type.values().find { type ->
+                type.name.lowercase() == typeName
+            } ?: Type.Unknown
+        }
+    }
+}
