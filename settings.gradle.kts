3b72180de77b0d5a57c67e647d5713fd6aa04e23
Anders Kielsholm <anders@anigif.dk>
2024-05-24 23:23:40 +0200

    Bump to Kotlin 2.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "sms-core"
 
 pluginManagement {
     plugins {
-        val kotlinVersion = "1.9.0"
+        val kotlinVersion = "2.0.0"
 
         kotlin("multiplatform") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
