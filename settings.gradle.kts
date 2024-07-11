e8363e7a8c0b904204aff9f3b166f7129870e98c
Anders Kielsholm <anders@anigif.dk>
2024-07-12 00:08:33 +0200

    Make sure the classes we use in swift are prepended with something sane instead of just "Library" as it was after making a module called library

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,3 +9,10 @@ pluginManagement {
 }
 
 include(":library")
+
+// This name will be used as module name by the generated iOS libraries (and for instance also as
+// name for the generated klibs) and will therefore be used as prefix for the Swift name of the
+// classes available in the swift-projects (when this library is included in a shared KMP module).
+// For instance can the Team interface be referenced as SmsCoreTeam.
+// Conclusion: Don't change this unless we want to change that prefix!
+project(":library").name = "SmsCore"
