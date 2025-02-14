062567454897000c68b2ea90e063259d2ac6a1cf
Anders Kielsholm <anders@anigif.dk>
2025-02-15 00:40:28 +0100

    Add as many targets as possible, but leave out a few that we won't use right away and won't be relevant as the intermediate projects of sms (as they for instance use Kermit)

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import java.net.URI
 
 group = "dk.spilpind"
@@ -12,18 +14,7 @@ plugins {
 }
 
 kotlin {
-    jvmToolchain(17)
-
-    jvm()
-
-    js(IR) {
-        nodejs {
-        }
-    }
-
-    iosArm64() // iOS device
-    iosSimulatorArm64() // iOS simulator (on Apple silicon machine)
-    iosX64() // iOS simulator (on Intel machine)
+    setupTargets()
 
     sourceSets {
         val commonMain by getting {
@@ -60,5 +51,50 @@ publishing {
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
+    }
+}
+
+
+private fun KotlinMultiplatformExtension.setupTargets() {
+    // We try to aim for as many targets as possible as we don't have any platform-specific code and just want to be as
+    // available as possible. The targets here are inspired by this:
+    // https://github.com/Kotlin/kotlinx.coroutines/blob/1.10.1/buildSrc/src/main/kotlin/kotlin-multiplatform-conventions.gradle.kts
+    // (WASI was removed as it isn't supported by Kermit at the moment)
+
+    jvm()
+    jvmToolchain(17)
+
+    iosArm64() // iOS device
+    iosSimulatorArm64() // iOS simulator (on Apple Silicon machine)
+    iosX64() // iOS simulator (on Intel machine)
+
+    linuxX64()
+    linuxArm64()
+    mingwX64() // 64-bit Windows 7 and later using MinGW compatibility layer
+    macosX64()
+    macosArm64() // Apple macOS (on Apple Silicon machine)
+
+    watchosArm32() // Apple watchOS on ARM32 platforms
+    watchosArm64() // Apple watchOS on ARM64 platforms with ILP32
+    watchosDeviceArm64() // Apple watchOS on ARM64 platforms
+    watchosSimulatorArm64() // Apple watchOS simulator (on Apple Silicon machine)
+    watchosX64() // Apple watchOS 64-bit simulator (on Intel machine)
+
+    tvosArm64() // Apple tvOS on ARM64 platforms
+    tvosSimulatorArm64() // Apple tvOS simulator (on Apple Silicon machine)
+    tvosX64() // Apple tvOS simulator (on Intel machine)
+
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
+
+    js {
+        nodejs()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        nodejs()
     }
 }
