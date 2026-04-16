6958962f2f1944f2b4fba87923fb0a741dac8e55
Anders Kielsholm <anders@anigif.dk>
2026-04-16 21:20:42 +0200

    Bump to version 1.6.0

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import java.net.URI
 
 group = "dk.spilpind"
-version = "1.6.0-dev"
+version = "1.6.0"
 val baseArtifactId = "sms-core"
 
 plugins {
